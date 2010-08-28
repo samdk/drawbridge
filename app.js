036c265c7416a14b7fc66509f9aeff2e837014e9
prasincs <prasannagautam@gmail.com>
2010-08-28 01:02:47 -0400

    addSketch creates a new row and hashes the id with a secret key

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var database = require('./config').database;
+var secret_key = "test";
 var sys = require('sys');
 var gently = new (require('gently'));
 var mysql= require('./lib/mysql');
+var crypto = require ('crypto'); //node.js needs to be compiled with openssl support
 var client = new mysql.Client();
 
 client.host = database.host;
@@ -12,28 +14,36 @@ client.database = database.name;
 
 
 client.connect();
-exports.Sketch = function (id){
-	
-	getSketch = function (id){
+exports.getSketch = function (id, runFunction){
 		client.query("select * from sketch where id = " + id, 
 			function selectCb(err, results, fields){
 				if (err){
 					throw err;
 				}
 				if (results.length ===1) {
-					return results[0];
+					runFunction(results);	
 				}
 			}
 		);
 
-	}
+};
 
+exports.addSketch = function (runFunction){
+	client.query ("INSERT INTO sketch values (NULL, '', NULL, NULL)",function selectCb(err, results, fields){
+		if (err){
+			throw err;
+		}
+		var insertId = results.insertId;
+		var hash = crypto.createHash("sha1").update(insertId+secret_key).digest("hex");
+		client.query("UPDATE sketch SET hash=? WHERE id=?", [hash, insertId], 
+		function selectCb(err, results, fields){
+			if (err){
+				throw err;
+			}
+			runFunction(hash);
+			//console.log(results);
+		}
+		);
+
+});
 }
-	
-
-	
-
-
-
-
-
