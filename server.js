72d34b24c30c1cee649b9b6d11c2461b87d341ae
prasincs <prasannagautam@gmail.com>
2010-08-28 03:05:39 -0400

    added test.html in views

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express'),
     app = require('./app');
 
 server.get('/', function(req,res) {
-	res.render('./public/index.html');
+	res.render('test.html');
 });
 
 server.get('/drawing', function(req,res){
