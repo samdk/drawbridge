c7db153a289792a301c30f93be4ae14f4ab5e1cc
prasincs <prasannagautam@gmail.com>
2010-08-28 03:03:11 -0400

    added rendering

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 require.paths.push('/home/node/.node_libraries');
 var express = require('express'),
-    server = express.createServer(),
+    server = express.createServer(
+    	express.logger(),
+	express.bodyDecoder()
+    ),
     app = require('./app');
 
 server.get('/', function(req,res) {
-	res.send('Test');
+	res.render('./public/index.html');
+});
+
+server.get('/drawing', function(req,res){
+	res.render('./public/drawing.html');
 });
 
 server.listen(80);
