b3efa250cd3918aee52dc8a221f791dab55fdcba
prasincs <prasannagautam@gmail.com>
2010-08-28 10:49:56 -0400

    adding the mysql schema for future

---
+++ sql/sketch_salad.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.2deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Aug 28, 2010 at 10:48 AM
+-- Server version: 5.1.41
+-- PHP Version: 5.3.2-1ubuntu4.2
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `sketch_salad`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `segment`
+--
+
+DROP TABLE IF EXISTS `segment`;
+CREATE TABLE `segment` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `sketch_id` bigint(20) NOT NULL,
+  `points` longtext NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `segment`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sketch`
+--
+
+DROP TABLE IF EXISTS `sketch`;
+CREATE TABLE `sketch` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `hash` varchar(40) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  KEY `hash` (`hash`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sketch`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sketch_to_segment`
+--
+
+DROP TABLE IF EXISTS `sketch_to_segment`;
+CREATE TABLE `sketch_to_segment` (
+  `sketch_id` bigint(20) NOT NULL,
+  `segment_id` bigint(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sketch_to_segment`
+--
+
