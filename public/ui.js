21dbf2c33d4531719829b8358148c41a18aa5ba0
Eli <eli@elilies.com>
2010-08-28 21:43:49 -0400

    YAY ELI

--- public/ui.js
+++ public/ui.js
@@ -2,19 +2,19 @@ var UI = {
     canvas : false,
     sign_on_user : function(user){
         var found = false;
-        console.log("Sign on user", user);
+
         $("#people ul li").each(function(){
-            console.log("uuid", $(this).data("uuid"));
             if($(this).data("uuid") == user.id){
                 found = true;
                 $(this).removeClass("offline");
+                $(this).text(user.name);
             }
         });
         if(!found){
             $("#people ul").append("<li>"+user.name+"</li>");
             $("#people ul li:last").data("uuid", user.id);            
         }
-            
+                    
         $("#people h2").text(
             ($("#people li").length-1) +
             ($("#people li").length == 2 ? ' person ' : ' people ') +
