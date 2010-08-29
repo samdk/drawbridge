0e51f53fce544e9752138c38053db81e9b6abd4c
Sam DeFabbia-Kane <sam@defabbiakane.com>
2010-08-29 02:55:57 -0400

    Stuff for revisions/merging.

--- public/drawing-tmp.js
+++ public/drawing-tmp.js
@@ -4,23 +4,27 @@ $(function(){
 	}
 	$("#variations ul a").click(function(){
 		//switch to the variation clicked on
-		console.log("clicked");
+		var canvas =  $(this).find("canvas");
+		window.location.hash = canvas.data("rev");
+		drawOnCanvas(getRevisionId());
 		return false;
 	});
 	$("#add").click(function(){
+		$(".mirror").removeClass("mirror");
+		var current = UI.variations(getRevisionId()),
+			newVariationId = cloneCurrent(getRevisionId());
 		$("#variations ul").prepend(function(){
-			var id = getNewId();
-			return '<li><a href="#" id="link'+id+'">'+'<canvas id="var'+id+'" width="120" height="90"></canvas></a></li>'
+			return '<li><a href="#"><canvas class="mirror" width="120" height="90"></canvas></a></li>'
 		});
-		$("#variations canvas").draggable({opacity: 0.7,
-										   revert: true,
-										   revertDuration: 200});
+		window.location.hash = newRevisionId;
+		UI.variations[newVariationId] = littleCanvas($(".mirror")[0]);
+		$(".mirror").draggable({opacity: 0.7,revert: true,revertDuration: 200}).
+					.data("rev",newVariationId);
 		return false;
 	});
 
-	$("#variations canvas").draggable({opacity: 0.7,
-									   revert: true,
-									   revertDuration: 200});
+	$(".mirror").draggable({opacity: 0.7,revert: true,revertDuration: 200})
+				.data("rev",getRevisionId());
 	$("#canvas-wrap").droppable({
 		drop: function(e,ui){ console.log("dropped #"+ui.draggable.attr("id"))}
 	});
