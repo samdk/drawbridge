81632662be2fb593de394dd13263574d5b165bc6
Sam DeFabbia-Kane <sam@defabbiakane.com>
2010-08-29 17:51:01 -0400

    Removed extraneous logging.

--- public/drawing-tmp.js
+++ public/drawing-tmp.js
@@ -7,8 +7,11 @@ $(function(){
 	$(".mirror").draggable({opacity: 0.7,revert: true,revertDuration: 200})
 				.data("rev",getRevisionId());
 	$("#canvas-wrap").droppable({
-		drop: function(e,ui){ 
-			CommLink.mergeVariation(ui.draggable.data("rev"));
+		drop: function(e,ui){
+			var rev = ui.draggable.data("rev");
+			if (rev != getRevisionId()) {
+				CommLink.mergeVariation(rev);
+			}
 		}
 	});
 });
